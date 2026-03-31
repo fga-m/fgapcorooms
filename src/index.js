f93388ee886a616a0646e3a1b6efb21eb94d4b8c
fga-m <media@fgam.org.au>
2026-03-31 15:46:36 +1100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app.jsx'; 
 
+/**
+ * src/index.js
+ * Standard React 18 entry point.
+ * We use the explicit .jsx extension for the App import 
+ * to satisfy strict ESM requirements on Vercel.
+ */
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
